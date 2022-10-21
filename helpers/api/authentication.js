17945c511897c98aa6c55fa676e7c3be2403d29c
gitkolihis <adelaniolanrewajuisrael@gmail.com>
2022-10-21 20:08:27 +0100

    Integrated Login

--- helpers/api/authentication.js
+++ helpers/api/authentication.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import qs from "qs";
 
 const URL = process.env.API_URL;
 
@@ -11,6 +12,20 @@ export const register = async (data) => {
     },
     data: JSON.stringify(data),
   };
+  const response = await axios(config);
+  return response;
+};
+
+export const login = async (data) => {
+  const config = {
+    method: "post",
+    url: `http://localhost:3000/auth/login`,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    data: qs.stringify(data),
+  };
+
   const response = await axios(config);
   return response;
 };
--- pages/login.js
+++ pages/login.js
@@ -3,10 +3,44 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "../styles/Home.module.css";
-import { LockClosedIcon } from "@heroicons/react/20/solid";
+import React, { useState } from "react";
+import Router from "next/router";
+import Spinner from "../components/spinner";
+import { login } from "../helpers/api/authentication";
 
 export default function Login() {
+  const [state, setState] = useState({
+    username: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const handleChange = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      const result = await login(state);
+      console.log("Login: ", result);
+      if (result.status === 200 || result.status === 201) {
+        Router.push("/dashboard");
+      }
+    } catch (e) {
+      setLoading(false);
+      setErrorMsg(e?.response?.data?.message);
+      setTimeout(() => {
+        setErrorMsg("");
+      }, 3000);
+    }
+  };
+
   return (
     <>
       <div className="bg-grey-lighter min-h-screen flex flex-col">
@@ -14,31 +48,41 @@ export default function Login() {
           <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
             <h1 className="mb-8 text-3xl text-center">Login</h1>
 
-            <input
-              type="text"
-              className="block border border-grey-light w-full p-3 rounded mb-4"
-              name="email"
-              placeholder="Email"
-            />
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                className="block border border-grey-light w-full p-3 rounded mb-4"
+                name="username"
+                placeholder="Email"
+                onChange={handleChange}
+              />
 
-            <input
-              type="password"
-              className="block border border-grey-light w-full p-3 rounded mb-4"
-              name="password"
-              placeholder="Password"
-            />
+              <input
+                type="password"
+                className="block border border-grey-light w-full p-3 rounded mb-4"
+                name="password"
+                placeholder="Password"
+                onChange={handleChange}
+              />
 
-            <button
-              type="submit"
-              className=" w-full mt-4 text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900"
-            >
-              Login
-            </button>
+              {errorMsg && (
+                <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  {errorMsg}
+                </label>
+              )}
+
+              <button
+                type="submit"
+                className=" w-full mt-4 text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900"
+              >
+                {loading ? <Spinner /> : "Login"}
+              </button>
+            </form>
           </div>
 
           <div className="text-grey-dark mt-6">
             Didn't have an account?
-            <Link href={"/"} >
+            <Link href={"/"}>
               <a className="text-pink-900 ml-2">Sign Up</a>
             </Link>
             .
