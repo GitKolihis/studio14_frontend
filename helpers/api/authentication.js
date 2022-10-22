aca8c18a783466b288952b2cd67826da7d5c419b
gitkolihis <adelaniolanrewajuisrael@gmail.com>
2022-10-22 07:44:41 +0100

    Added Movie Search

--- helpers/api/authentication.js
+++ helpers/api/authentication.js
@@ -29,3 +29,16 @@ export const login = async (data) => {
   const response = await axios(config);
   return response;
 };
+
+export const search = async (data) => {
+  const config = {
+    method: "get",
+    url: `http://localhost:3000/movieapi/title?title=${data.q}`,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  };
+
+  const response = await axios(config);
+  return response;
+};
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,24 +1,61 @@
+import React, { useState } from "react";
+import Spinner from "../components/spinner";
+import { search } from "../helpers/api/authentication";
+
 export default function Dashboard() {
+  const [state, setState] = useState({
+    q: "",
+  });
+  const [loading, setLoading] = useState(false)
+  const [movies, setMovies] = useState([])
+
+  const handleChange = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      const result = await search(state);
+      // console.log("Search Result:: ", result.data?.data?.result)
+      setMovies(result.data?.data?.result)
+      setLoading(false)
+    } catch (e) {
+      console.log(e);
+      setLoading(false)
+    }
+  };
+
   return (
     <>
-      <div className="flex">
-        <aside className="w-44 fixed left-0 top-0 h-screen bg-slate-700 p-10">
-          <h1 className="text-white text-4xl">Sidebar</h1>
-        </aside>
-        <main className="flex-1 ml-44">
-          <div className="h-96 bg-amber-400 p-10">
-            <h1 className="text-4xl">Top Content</h1>
+      <div className="bg-grey-lighter min-h-screen flex">
+        <div className="container max-w-lg mx-auto flex-1 flex flex-col items-center justify-center px-2">
+          <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+            <h1 className="mb-8 text-3xl text-center">Movie Search</h1>
+
+            <form onSubmit={handleSubmit}>
+              <div className="flex flex-row">
+                <input
+                  type="text"
+                  className="block border border-grey-light w-full p-3 rounded mb-4"
+                  name="q"
+                  placeholder="Seach for movie title"
+                  onChange={handleChange}
+                />
+                 <button
+                type="submit"
+                className=" ml-3 rounded-full w-full text-white bg-yellow-400 hover:bg-yellow-600 focus:outline-none  focus:ring-yellow-300 font-medium text-sm px-2 py-2.5 text-center mr-2 mb-4 dark:focus:ring-yellow-900"
+              >
+                {loading ? <Spinner /> : "Search Movie"}
+              </button>
+              </div>
+            </form>
           </div>
-          <div className="h-96 bg-white p-10">
-            <h1 className="text-4xl">Middle Content</h1>
-          </div>
-          <div className="h-96 bg-green-400 p-10">
-            <h1 className="text-4xl">Middle Content</h1>
-          </div>
-          <div className="h-96 bg-indigo-400 p-10">
-            <h1 className="text-4xl">Last Content</h1>
-          </div>
-        </main>
+        </div>
       </div>
     </>
   );
